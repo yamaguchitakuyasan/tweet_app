4d310096b12bee972d0c122071a0db5a2556f800
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-28 21:15:30 +0900

    secound commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,12 @@ Rails.application.routes.draw do
   post "posts/:id/destroy" => "posts#destroy"
   get "/" => "home#top"
   get "about" => "home#about"
+
+  get "/users/index" => "users#index"
+  get "/users/:id" => "users#show"
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
